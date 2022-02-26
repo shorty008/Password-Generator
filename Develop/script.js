ad91790b25a903c624840270516fb8691e2c2544
Robbie K <rkahl08@gmail.com>
2022-02-26 10:15:30 -0500

    buiding out the Password length Crieria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,22 @@
 // Assignment code here
 
 
+//Password Length Function 
+var passwordNumber = function() { 
+  passwordNumberTotal = window.prompt("Please enter the length of password you want to create between 8 and 128 characters")
+
+
+    //validate that the password length is between 8 and 
+
+        if (passwordNumberTotal > 7 && passwordNumberTotal < 129) {
+          alert ("You have chosen a password of " + passwordNumberTotal + "!" )
+        }
+
+        else {
+          passwordNumber();
+        }
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 

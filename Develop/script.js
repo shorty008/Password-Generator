17f87dc180f3c72e3ca713f9369c077c2bd60d3c
Robbie K <rkahl08@gmail.com>
2022-02-26 10:28:07 -0500

    buiding out the Password length Crieria

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,37 @@
 // Assignment code here
+var specialCharacters = " .\+*?[^]$(){}=!<>|:-";
+var lowerCaseAphabet = "abcdefghijkmnopqrstuvwxyz";
+var capitalAphabet = "ABCDEFGHJKMNOPQRSTUVWXYZ";
+var numbersList = "0123456789"
 
 
 //Password Length Function 
-var passwordNumber = function() { 
-  passwordNumberTotal = window.prompt("Please enter the length of password you want to create between 8 and 128 characters")
+var passwordNumberCriteria = function() { 
+  passwordNumberCriteriaTotal = window.prompt("Please enter the length of password you want to create between 8 and 128 characters");
 
 
     //validate that the password length is between 8 and 
 
-        if (passwordNumberTotal > 7 && passwordNumberTotal < 129) {
-          alert ("You have chosen a password of " + passwordNumberTotal + "!" )
+        if (passwordNumberCriteriaTotal > 7 && passwordNumberCriteriaTotal < 129) {
+          alert ("You have chosen a password of " + passwordNumberCriteriaTotal + "!" );
         }
 
         else {
-          passwordNumber();
+          passwordNumberCriteria();
         }
 
+        passwordNumberCriteria = passwordNumberCriteriaTotal;
+
+      };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  passwordNumberCriteria();
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 

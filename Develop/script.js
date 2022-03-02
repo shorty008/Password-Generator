26d14e1fc4ca29ce17ef4a150c73f7420a29aab1
Robbie K <rkahl08@gmail.com>
2022-03-02 14:19:28 -0600

    built out the characters feature

--- Develop/script.js
+++ Develop/script.js
@@ -17,26 +17,81 @@ var passwordNumberCriteria = function() {
 
         passwordNumberCriteria = passwordNumberCriteriaTotal;
 
-      };
+};
 
 
-// Password Character Types Function
+var passwordCharacterCriteria = function (){
+  var passwordCharacters ="";
 
-  var specialCharacters = " .\+*?[^]$(){}=!<>|:-";
-  var lowerCaseAphabet = "abcdefghijkmnopqrstuvwxyz";
-  var capitalAphabet = "ABCDEFGHJKMNOPQRSTUVWXYZ";
-  var numbersList = "0123456789"
+  passwordCharacters = passwordCapitalLetters();
+  passwordCharacters = passwordLowerLetters(passwordCharacters);
+  passwordCharacters = passwordNumbers(passwordCharacters);
+  passwordCharacters = passwordSpecialChar(passwordCharacters);
 
-
-
-//Generate Password 
-
-var generatePassword = function() {
-  for(var 1 = 0; i < passwordNumberCriteriaTotal; i++) {
-    
+  if (passwordCharacters === "" || passwordCharacters === null) {
+    alert("Try again! Please select at least one password of the password criteria");
+    return passwordCharacter();
   }
-}
+  else {
+    alert("You password will contain a random selection containing the following characers:  " + passwordCharacters);
+    return passwordCharacters;
+  }
+};
 
+ //Capital Letters Code    
+  var passwordCapitalLetters =function(){
+    var capitalLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var capitalLettersPrompt = prompt("Enter Yes if you want to use Capital Letters");
+      if (capitalLettersPrompt === "Yes" || capitalLettersPrompt === "yes" || capitalLettersPrompt === "YES"){
+        return capitalLetters;
+      }
+      else {
+        capitalLetters = ""
+        return capitalLetters;
+      }
+    };
+
+//Lower Case Letters Code    
+var passwordLowerLetters =function(passwordCharacter){
+  var lowerLetters ="abcdefghijklmnopqrstuvwxyz";
+  var lowerLettersPrompt = prompt("Enter Yes if you want to use Lower Case Letters");
+      if (lowerLettersPrompt === "Yes" || lowerLettersPrompt === "yes" || lowerLettersPrompt === "YES"){       
+        lowerLetters = lowerLetters + passwordCharacter;
+        return lowerLetters;
+      }
+      else {
+        lowerLetters = passwordCharacter;
+        return lowerLetters;
+      }     
+    };
+
+//generate number character code    
+var passwordNumbers =function(passwordCharacter){
+  var numbers ="0123456789";
+  var numbersPrompt = prompt("Enter Yes if you want to use Numbers");
+      if (numbersPrompt === "Yes" || numbersPrompt === "yes" || numbersPrompt === "YES"){       
+        numbers = numbers + passwordCharacter;
+        return numbers;
+      }
+      else {
+        numbers = passwordCharacter;
+        return numbers;
+      }  
+    };
+
+//generate special character code    
+var passwordSpecialChar =function(passwordCharacter){
+  var specialChar ="!@#$%^&*()";
+  var specialCharPrompt = prompt("Enter Yes if you want to use Special Characaters");
+  if (specialCharPrompt === "Yes" || specialCharPrompt === "yes"){          
+      specialChar = specialChar + passwordCharacter;
+        return specialChar;
+      }
+      else {
+        specialChar = passwordSpecialChar;
+        return specialChar;     
+    };
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -46,6 +101,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   passwordNumberCriteria();
+  passwordCharacterCriteria();
 
 
   var password = generatePassword();
